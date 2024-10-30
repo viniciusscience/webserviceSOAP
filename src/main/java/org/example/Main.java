a434c3eae67e8767a7f60f499f3e24009bada268
Vinicius <daftwarz@gmail.com>
2024-10-29 21:01:13 -0300

    ajuste

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,61 @@
 package org.example;
 
 
-import sngpc.webservice.ValidarUsuario;
-import sngpc.webservice.ValidarUsuarioResponse;
+import xsd2java.CtInsumoBasico;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    private static final String EMAIL = "teste@anvisa.gov.br";
-    private static final String PASSWORD = "12345678";
+    private static final String EMAIL = "teste@anvisa.go22v.br";
+    private static final String PASSWORD = "123456782";
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
+//
+//        var wsSngpc = new SngpcIntegrationService();
+//
+//        var validarUsuario = new ValidarUsuario();
+//
+//        validarUsuario.setEmail(EMAIL);
+//        validarUsuario.setSenha(PASSWORD);
+//
+//        var result = wsSngpc.validarUsuario(validarUsuario.getEmail(), validarUsuario.getSenha());
+//
+//
+//        var validarUsuarioResponse = new ValidarUsuarioResponse();
+//
+//      validarUsuarioResponse.setValidarUsuarioResult(result);
+//
+//        System.out.println(validarUsuarioResponse.getValidarUsuarioResult());
 
-        var wsSngpc = new SngpcIntegrationService();
 
-        var validarUsuario = new ValidarUsuario();
+            // Criação do objeto
+            CtInsumoBasico insumo = new CtInsumoBasico();
+            insumo.setCodigoInsumo("00092");
+            insumo.setNumeroLoteInsumo("A315");
+            insumo.setInsumoCNPJFornecedor("99900099900000");
+            insumo.setQuantidadeInsumo(new BigDecimal("300000.0"));
+            insumo.setTipoUnidade("1");
 
-        validarUsuario.setEmail(EMAIL);
-        validarUsuario.setSenha(PASSWORD);
+            // Converter o objeto em XML
+            JAXBContext jaxbContext = JAXBContext.newInstance(CtInsumoBasico.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-        var result = wsSngpc.validarUsuario(validarUsuario.getEmail(), validarUsuario.getSenha());
+            StringWriter sw = new StringWriter();
+            marshaller.marshal(insumo, sw);
+            String xml = sw.toString();
+
+            System.out.println("XML gerado:\n" + xml);
 
 
-        var validarUsuarioResponse = new ValidarUsuarioResponse();
-
-        validarUsuarioResponse.setValidarUsuarioResult(result);
-
-        System.out.println(validarUsuarioResponse.getValidarUsuarioResult());
     }
 
 
--- src/main/java/org/example/SngpcIntegrationService.java
+++ src/main/java/org/example/SngpcIntegrationService.java
@@ -1,53 +1,53 @@
-package org.example;
-
-import sngpc.webservice.*;
-
-import java.util.function.Supplier;
-
-public class SngpcIntegrationService implements SngpcSoap {
-
-    private static final SngpcSoap wsSngpc;
-
-    static {
-        try {
-            wsSngpc = new Sngpc().getSngpcSoap();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    @Override
-    public String enviaArquivoSNGPC(String email, String senha, byte[] arq, String hashIdentificacao) {
-
-        return tryAndGetResult(() -> wsSngpc.enviaArquivoSNGPC(email, senha, arq, hashIdentificacao));
-
-
-    }
-
-    @Override
-    public String validarUsuario(String email, String senha) {
-
-        return tryAndGetResult(() -> wsSngpc.validarUsuario(email, senha));
-
-
-    }
-
-    @Override
-    public String consultaDadosArquivoSNGPC(String email, String senha, String cnpj, String hash) {
-        return tryAndGetResult(() -> wsSngpc.consultaDadosArquivoSNGPC(email, senha, cnpj, hash));
-
-
-    }
-
-
-    private <T> T tryAndGetResult(Supplier<T> supplier) {
-
-        try {
-            return supplier.get();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
-
+//package org.example;
+//
+//import sngpc.webservice.*;
+//
+//import java.util.function.Supplier;
+//
+//public class SngpcIntegrationService implements SngpcSoap {
+//
+//    private static final SngpcSoap wsSngpc;
+//
+//    static {
+//        try {
+//            wsSngpc = new Sngpc().getSngpcSoap();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
+//
+//    @Override
+//    public String enviaArquivoSNGPC(String email, String senha, byte[] arq, String hashIdentificacao) {
+//
+//        return tryAndGetResult(() -> wsSngpc.enviaArquivoSNGPC(email, senha, arq, hashIdentificacao));
+//
+//
+//    }
+//
+//    @Override
+//    public String validarUsuario(String email, String senha) {
+//
+//        return tryAndGetResult(() -> wsSngpc.validarUsuario(email, senha));
+//
+//
+//    }
+//
+//    @Override
+//    public String consultaDadosArquivoSNGPC(String email, String senha, String cnpj, String hash) {
+//        return tryAndGetResult(() -> wsSngpc.consultaDadosArquivoSNGPC(email, senha, cnpj, hash));
+//
+//
+//    }
+//
+//
+//    private <T> T tryAndGetResult(Supplier<T> supplier) {
+//
+//        try {
+//            return supplier.get();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
+//
